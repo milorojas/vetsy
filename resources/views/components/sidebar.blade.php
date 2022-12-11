1091ffa9baaba5a24847aab480c11151178c61a5
Camilo Rojas <camilo.rojas@outlook.jp>
2022-12-11 02:21:35 -0300

    Add some WIP links on admin page

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="col-span-12 px-4 sm:col-span-6 md:col-span-4 lg:col-span-2 border-r">
+<div class="bg-white col-span-12 px-4 sm:col-span-6 md:col-span-4 lg:col-span-2 border-r">
     <nav class="space-y-1" aria-label="Sidebar">
         <x-sidebar-group :name="__('Authorization')">
             <x-sidebar-link href="{{ route('dashboard') }}" :active="request()->routeIs('')" :total="20">
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="grow py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden sm:rounded-lg">
                 <x-jet-welcome />
